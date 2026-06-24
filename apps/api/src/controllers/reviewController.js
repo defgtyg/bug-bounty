533f3fe5dfd6b6879075eaa54d0067f9694459b5
defgtyg <141336881+defgtyg@users.noreply.github.com>
2026-06-24 11:11:58 +0800

    Validate review request body
    Parse review create payloads before storing reviews.

--- apps/api/src/controllers/reviewController.js
+++ apps/api/src/controllers/reviewController.js
@@ -1,10 +1,12 @@
 import { ok } from "../utils/response.js";
 import { createReview, listReviews } from "../services/reviewService.js";
+import { createReviewSchema } from "../validators/marketplace.js";
 
 export async function getReviews(req, res) {
   return ok(res, await listReviews());
 }
 
 export async function postReview(req, res) {
-  return ok(res, await createReview(req.body), 201);
+  const payload = createReviewSchema.parse(req.body);
+  return ok(res, await createReview(payload), 201);
 }
